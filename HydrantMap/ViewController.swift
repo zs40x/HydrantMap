e4b315c11de63bce19ac2207afab35a8b3e31183
Stefan Mehnert <StefanMehnert@gmail.com>
2017-02-02 21:00:46 +0100

    First commit

---
+++ HydrantMap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HydrantMap
+//
+//  Created by Stefan Mehnert on 02/02/2017.
+//  Copyright © 2017 Stefan Mehnert. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
