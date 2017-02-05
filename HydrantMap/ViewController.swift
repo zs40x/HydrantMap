467cf00f58ed2febce116595f0d41df48bf59a29
Stefan Mehnert <StefanMehnert@gmail.com>
2017-02-05 14:56:39 +0100

    Added mapView outlet

--- HydrantMap/ViewController.swift
+++ HydrantMap/ViewController.swift
@@ -15,6 +15,8 @@ struct Hydrant {
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
